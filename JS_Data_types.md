fc7765e9839de7aef5af38ed2c2d836337dbca1f
Hero Supangat <hero.supangat@Supporting.corp.bankmandiri.co.id>
2018-03-22 09:59:33 +0700

    add_for_branch_JS_DT

---
+++ JS_Data_types.md
@@ -0,0 +1,11 @@
+#JS Data Types
+
+## Primitive Data Types
+- Number
+- String
+## Non-Primitive Data Types
+- Object
+- Array
+### Spesial Data Types
+- Null
+- Undefined
---
+++ JavaScriptDataType.js
@@ -0,0 +1,15 @@
+var A;
+let B;
+const C = 0;
+
+function name(ini){
+    const D = []
+    let E = {}
+    console.log(typeof ini)
+    console.log(typeof D)
+    console.log(typeof E)
+}
+
+name('ini')
+
+// ini namanya comment
